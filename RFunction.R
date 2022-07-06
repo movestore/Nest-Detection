6a23a7e968b53948f824b16920b037a4f8bfd5db
Andrea Kölzsch <65662928+andreakoelzsch@users.noreply.github.com>
2022-07-06 14:11:43 +0200

    solve as.POSIX error

--- RFunction.R
+++ RFunction.R
@@ -83,13 +83,11 @@ rFunction = function(data, sea.start="2000-01-01", sea.end="2000-12-31", nest.cy
       nest.data <- foreach (nest.table.b = nest.table.df$uburst) %do% {
         nest.table.i <- nest.table.df[nest.table.df$uburst==nest.table.b,]
         datai <- data.split[[which(names(data.split)==nest.table.i$individual.local.identifier)]] #these names are trackIds of the incoming data set, ok
-        nest.data.i <- datai[timestamps(datai)>=nest.table.i$first_date & timestamps(datai)<(nest.table.i$last_date+1)] #so, we get the track of each nesting attempt, i.e. quite some duplicate data (but ok, so that can visualise them in next App)
+        nest.data.i <- datai[timestamps(datai)>=as.POSIXct(nest.table.i$first_date) & timestamps(datai)<(as.POSIXct(nest.table.i$last_date)+1)] #so, we get the track of each nesting attempt, i.e. quite some duplicate data (but ok, so that can visualise them in next App)
         nest.data.i
       }
       names(nest.data) <- nest.table.df$uburst #names are the breeding attempt unique burst IDs
-      
-      #nest.data <- c(nest.data,"nest"=nest.table.move)
-      
+
       nest.data.nozero <- nest.data[unlist(lapply(nest.data, length) > 0)]
       result <- moveStack(nest.data.nozero,forceTz="UTC") #return track segments in breeding modus
     }
